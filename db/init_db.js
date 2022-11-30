2e7d19ccbb3a0af95275080076554ad398f864c2
Austin Lawrence <austin.lawrence.al@gmail.com>
2022-11-29 23:33:18 -0600

    added more test jobs to the database; tested scroll behavior of dblist when longer than window height; fixed sticky footer issue by setting specific height on database and login component, will need to do a better fix at some point

--- db/init_db.js
+++ db/init_db.js
@@ -66,6 +66,20 @@ async function populateInitialData() {
         {jobNumber: 'TER-321', location: 'Sachse, TX', numHoles: 1, numFeet: 20, rigId: 2},
         {jobNumber: 'AAA-111', location: 'Parker, TX', numHoles: 5, numFeet: 1000, rigId: 1},
         {jobNumber: 'ZZZ-2626', location: 'Sea of Tranquility, Moon', numHoles: 3, numFeet: 120, rigId: 4},
+        {jobNumber: 'ZZZ-2627', location: 'Istanbul, Turkey', numHoles: 2, numFeet: 100, rigId: 4},
+        {jobNumber: 'ZZZ-2628', location: 'Berlin, Germany', numHoles: 4, numFeet: 80, rigId: 4},
+        {jobNumber: 'ZZZ-2629', location: 'Vancouver, Canada', numHoles: 4, numFeet: 80, rigId: 4},
+        {jobNumber: 'ZZZ-2630', location: 'Vancouver, Washington', numHoles: 3, numFeet: 60, rigId: 4},
+        {jobNumber: 'RYS-26', location: 'NY, NY', numHoles: 5, numFeet: 200, rigId: 4},
+        {jobNumber: 'I-2', location: 'Boston, MA', numHoles: 2, numFeet: 30, rigId: 4},
+        {jobNumber: 'Z-6', location: 'Sacramento, CA', numHoles: 2, numFeet: 40, rigId: 4},
+        {jobNumber: 'WELL-666', location: 'Chicago, IL', numHoles: 1, numFeet: 20, rigId: 4},
+        {jobNumber: 'ABFC-123', location: 'Ft. Worth, TX', numHoles: 3, numFeet: 60, rigId: 4},
+        {jobNumber: 'DEF-456', location: 'Dallas, TX', numHoles: 5, numFeet: 100, rigId: 4},
+        {jobNumber: 'GHI-100', location: 'Nowhere, KS', numHoles: 2, numFeet: 50, rigId: 4},
+        {jobNumber: 'GHI-200', location: 'St. Louis, MO', numHoles: 2, numFeet: 50, rigId: 4},
+        {jobNumber: 'GHI-300', location: 'Honolulu, Hawaii', numHoles: 2, numFeet: 50, rigId: 4},
+        {jobNumber: 'GHI-7400', location: 'Deep Space', numHoles: 2, numFeet: 50, rigId: 4},
       ];
 
       console.log("creating jobs");
