d866d42ff812e28d0400cc710418d7a5fb39a4cb
Austin Lawrence <austin.lawrence.al@gmail.com>
2023-04-23 18:11:53 -0500

    more experimentation with the map and custom map markers; downloaded a png with transparency and used it for custom marker testing

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,6 +1,8 @@
 import React, { useState, useRef, useEffect, useMemo, useCallback } from 'react';
 
 import '../mapquest/mapquest.js';
+//import a custom icon image
+import homeIcon from '../mapquest/transparent-home.png';
 
 import { getAllJobs, getAllRigs } from '../axios-services/index.js';
 
@@ -40,7 +42,7 @@ const MapView = ({token}) => {
   // I haven't figured out a way around the window.onload, but I'm sure there is one
   useEffect(() => {
     const reloadCount = sessionStorage.getItem('reloadCount');
-    if(reloadCount < 2) {
+    if(reloadCount < 1) {
       sessionStorage.setItem('reloadCount', String(reloadCount + 1));
       window.location.reload();
     } else {
@@ -72,60 +74,54 @@ const MapView = ({token}) => {
         return result;
       }
       
-      // api call to the geocoding, can send an array of comma-separated addresses to find the lat/lng
+      // api call to the geocoding, send an array of comma-separated addresses to find the lat/lng
       // then can use the lat/lng to create markers for the map
+      // if the job has a lat/lng instead of an address, will need to skip this somehow
+      // [32.888, -96.89615]
+      // ['2430 Merrell Rd #103, Dallas, TX 75229']
+
       const geocoder = L.mapquest.geocoding();
-      geocoder.geocode(['2430 Merrell Rd #103, Dallas, TX 75229'], geocodingCallback).then((response) => {
-        if (response.info.statuscode != 0) {
-          throw Error(`geocode failed, statuscode: ${response.info.statuscode}`);
-        }
-        console.log('full geocode response', response);
-        return response;
-      }).then((response) => {
-
+      geocoder.geocode(['2430 Merrell Rd #103, Dallas, TX 75229'], geocodingCallback)
         // extract the desired part of the geocode result - latLng - for placing the marker
-        const latlong = response.results[0].locations[0].latLng;
-        console.log('latlong in first .then', latlong);
-        return latlong;
+        .then((response) => {
+          const latlong = response.results[0].locations[0].latLng;
+          console.log('latlong in first .then', latlong);
+          return latlong;
+        })
+        // add map markers based on the geocode results
+        .then((latlong) => {
+          console.log('latlong in second .then', latlong);
+          
+          // adding a map marker and a pop-up attached to it at [37.7749, -122.4194]
+          // can change type and style the individual map marker, will make one style for each rig and apply dynamically
+          const marker = L.marker(latlong, {
+            icon: L.mapquest.icons.flag({
+              primaryColor: '#22407F',
+              secondaryColor: '#3B5998',
+              shadow: true,
+              size: 'md',
+              symbol: 'BASE'
+            })
+          }).addTo(map);
+          marker.bindPopup("<b>StrataBore Office</b><br>I am a popup. This is the main office location");
+          
+          // custom icon example
+          var smallMarker = L.icon({
+            iconUrl: homeIcon,
+            iconSize: [36, 36],
+            iconAnchor: [18, 36],
+            popupAnchor: [1, -36]
+          });
+          const customMarker = L.marker([32.900868, -97.044091], {
+            icon: smallMarker
+          }).addTo(map);
+            customMarker.bindPopup("<b>Custom Marker</b><br>I am a popup. This is a custom made marker");
 
-      }).then((latlong) => {
-        console.log('latlong in second .then', latlong);
 
-        // custom icon?
-        var markerSize = {'sm': [28, 35], 'md': [35, 44], 'lg': [42, 53]};
-        var markerAnchor = {'sm': [14, 35], 'md': [17, 44], 'lg': [21, 53]};
-        var markerPopupAnchor = {'sm': [1, -35], 'md': [1, -44], 'lg': [2, -53]};
-
-        var smallMarker = L.icon({
-          iconUrl: 'https://www.clipartmax.com/png/small/224-2245749_simple-house-icon-home-icon-vector-png.png',
-          iconSize: markerSize.lg,
-          iconAnchor: markerAnchor.lg,
-          popupAnchor: markerPopupAnchor.lg
-        });
-
-        // adding a map marker and a pop-up attached to it at [37.7749, -122.4194]
-        // can style the individual map marker, will make one style for each rig and apply dynamically
-        const marker = L.marker(latlong, {
-          icon: L.mapquest.icons.flag({
-            primaryColor: '#22407F',
-            secondaryColor: '#3B5998',
-            shadow: true,
-            size: 'md',
-            symbol: 'BASE'
           })
-        }).addTo(map);
-        marker.bindPopup("<b>StrataBore Office</b><br>I am a popup. This is the main office location");
-
-        const customMarker = L.marker([32.900868, -97.044091], {
-          icon: smallMarker
-        }).addTo(map);
-          customMarker.bindPopup("<b>Custom Marker</b><br>I am a popup. This is a custom made marker");
-
-
-        }).catch((error) => {
-          console.error(error);
-        });     
-      
+          .catch((error) => {
+            console.error(error);
+          });      
     }
   }
   mapInitialize();
