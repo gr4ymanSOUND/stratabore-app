fc75bfdd00d32b5100e2d56bf4f00558f2babfa0
Austin Lawrence <austin.lawrence.al@gmail.com>
2024-12-30 10:45:37 -0600

    set up a button list to select between the tools in the admin page. this will be useful later when we need to add more tools later down the line

--- src/components/AdminTools.js
+++ src/components/AdminTools.js
@@ -9,27 +9,37 @@ import { getAllUsers, getAllRigs } from '../axios-services';
 
 const AdminTools = ({token, user}) => {
 
-
+  const [whichTool, setWhichTool] = useState("usertool");
   // add a top bar to choose between tools: all users, rigs, and reports
   // potentially move button bar for each tool to this page
   // create reports component to use later
+
+  const toolSelectButton = useCallback((e) => {
+    console.log('tool clicked', e.target.id, e.target.className);
+    setWhichTool(e.target.id);
+  }, []);
   
   return (
     <>
-      {
-        user.isAdmin ? (
-          <div className='admin-tools'>
-            <i className="fa-solid fa-users"></i>
-            <div className='tool'>
-              <UserDatabase token={token}/>
-            </div>
-            <i className="fa-solid fa-truck-field"></i>
-            <div className='tool'>
-              <RigDatabase token={token}/>
-            </div>
-          </div>
-        ) : null
-      }
+    <div className='admin-tools'>
+      <div className="button-list tool-selector">
+        <button id='usertool' className='tool-button' onClick={toolSelectButton}>
+          <i id='usertool' className="fa-solid fa-users"></i>
+        </button>
+        <button className='tool-button' id='rigtool' onClick={toolSelectButton}>
+          <i id='rigtool' className="fa-solid fa-truck-field"></i>
+        </button>
+      </div>
+      <div className='tool'>
+        {
+          whichTool == "usertool" ? (
+                <UserDatabase token={token}/>
+          ) : (
+                <RigDatabase token={token}/>
+          )
+        }
+        </div>
+    </div>
     </>
 )
 
